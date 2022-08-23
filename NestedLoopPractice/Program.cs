3e898b500c4f5a87de1087a90670ef8527757690
Fred Chuks <Fred Chuks@FRED-CHUKS>
2022-08-23 05:59:45 +0100

    Add project files.

---
+++ NestedLoopPractice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoopPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] j = { "    *", "   ***", " *******", "**********" };
+            foreach (var h in j)
+            {
+
+
+                Console.WriteLine(h);
+                
+
+
+            }
+
+
+
+            
+
+            Console.ReadLine();
+        }
+        
+    }
+}
